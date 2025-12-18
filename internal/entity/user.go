cad37f8d4a3e2a9a3846a98cfa9acdb19a0ba998
Fathurrahman-R <mr.darksot123@gmail.com>
2025-12-18 10:18:22 +0700

    add: adding index on email column

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -6,6 +6,6 @@ type User struct {
 	gorm.Model
 	Fullname string `gorm:"not null; size:100"`
 	Username string `gorm:"type:varchar(100)"`
-	Email    string `gorm:"type:varchar(100)"`
+	Email    string `gorm:"uniqueIndex; not null; size:100"`
 	Password string `gorm:"size:255"`
 }
