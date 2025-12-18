1d4af7ac75f77d956ac4c1ab6869ee6805ee0399
Fathurrahman-R <mr.darksot123@gmail.com>
2025-12-18 10:22:29 +0700

    fix: change return type from entity pointer to entity new value

--- internal/user/repository_gorm.go
+++ internal/user/repository_gorm.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"api-digital-scoring/internal/entity"
+	"api-digital-scoring/internal/helper"
 	"context"
 	"errors"
 
@@ -16,7 +17,7 @@ func NewUserRepo(db *gorm.DB) Repository {
 	return &userRepo{db}
 }
 
-func (u userRepo) GetByUsername(ctx context.Context, username string) (*entity.User, error) {
+func (u userRepo) GetByUsername(ctx context.Context, username string) (entity.User, error) {
 	var user entity.User
 
 	err := u.db.WithContext(ctx).
@@ -24,40 +25,47 @@ func (u userRepo) GetByUsername(ctx context.Context, username string) (*entity.U
 		First(&user).Error
 
 	if err != nil {
-		return nil, err
+		return entity.User{}, err
 	}
 
-	return &user, nil
-}
-
-func (u userRepo) GetById(ctx context.Context, id string) (*entity.User, error) {
-	var user entity.User
-
-	err := u.db.WithContext(ctx).Where("id = ?", id).First(&user).Error
-	if err != nil {
-		return nil, err
-	}
-	return &user, nil
-}
-
-func (u userRepo) Create(ctx context.Context, user *entity.User) (*entity.User, error) {
-	uEmail := u.db.WithContext(ctx).Where("email = ?", user.Email).First(&user).Error
-	if uEmail == nil {
-		return nil, errors.New("email already in use")
-	}
-
-	err := u.db.WithContext(ctx).Create(&user).Error
-	if err != nil {
-		return nil, err
-	}
 	return user, nil
 }
 
-func (u userRepo) Update(ctx context.Context, user *entity.User) (*entity.User, error) {
-	err := u.db.WithContext(ctx).Save(&user).Error
+func (u userRepo) GetById(ctx context.Context, id string) (entity.User, error) {
+	var user entity.User
+
+	err := u.db.WithContext(ctx).Where("id = ?", id).First(&user).Error
+
 	if err != nil {
-		return nil, err
+		return helper.UserNotFound(err)
 	}
+
+	return user, nil
+}
+
+func (u userRepo) Create(ctx context.Context, user entity.User) (entity.User, error) {
+	uEmail := u.db.WithContext(ctx).Where("email = ?", &user.Email).First(&user).Error
+
+	if uEmail == nil {
+		return user, errors.New("email already in use")
+	}
+
+	err := u.db.WithContext(ctx).Create(&user).Error
+
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	return user, nil
+}
+
+func (u userRepo) Update(ctx context.Context, user entity.User) (entity.User, error) {
+	err := u.db.WithContext(ctx).Save(&user).Error
+
+	if err != nil {
+		return entity.User{}, err
+	}
+
 	return user, nil
 }
 
@@ -65,8 +73,10 @@ func (u userRepo) Delete(ctx context.Context, id string) error {
 	var user entity.User
 
 	err := u.db.WithContext(ctx).Where("id = ?", id).Delete(&user).Error
+
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
