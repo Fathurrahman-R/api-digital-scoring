2a3495e1a37a6097427c0f789198995940908125
Fathurrahman-R <mr.darksot123@gmail.com>
2025-12-18 10:22:39 +0700

    fix: change return type from entity pointer to entity new value

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -6,9 +6,9 @@ import (
 )
 
 type Repository interface {
-	GetByUsername(ctx context.Context, username string) (*entity.User, error)
-	GetById(ctx context.Context, id string) (*entity.User, error)
-	Create(ctx context.Context, user *entity.User) (*entity.User, error)
-	Update(ctx context.Context, user *entity.User) (*entity.User, error)
+	GetByUsername(ctx context.Context, username string) (entity.User, error)
+	GetById(ctx context.Context, id string) (entity.User, error)
+	Create(ctx context.Context, user entity.User) (entity.User, error)
+	Update(ctx context.Context, user entity.User) (entity.User, error)
 	Delete(ctx context.Context, id string) error
 }
