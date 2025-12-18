ab0dcea8f76e094747b1a2a7aff886abc0396da2
Fathurrahman-R <mr.darksot123@gmail.com>
2025-12-18 10:27:29 +0700

    change: changing private prefix from /api to / refactor: seperated entity and response binding dto to service layer

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -1,12 +1,11 @@
 package user
 
 import (
-	"api-digital-scoring/internal/entity"
+	"api-digital-scoring/internal/helper"
 	"api-digital-scoring/internal/user/dto"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/bcrypt"
 )
 
 type Handler struct {
@@ -20,54 +19,44 @@ func NewHandler(service *Service) *Handler {
 }
 
 func (h *Handler) RegisterRoutes(r *gin.RouterGroup) {
-	r.POST("/user", h.Create)
-	r.PUT("/user", h.Update)
-	r.GET("/user/:id", h.GetById)
-	r.DELETE("/user/:id", h.Delete)
+	r.POST("user", h.Create)
+	r.PUT("user", h.Update)
+	r.GET("user/:id", h.GetById)
+	r.DELETE("user/:id", h.Delete)
 }
 
 func (h *Handler) GetById(c *gin.Context) {
+
 	u, err := h.service.GetById(c, c.Param("id"))
+
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"errors": helper.NewErrorResponse(err.Error())})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"data": dto.Response{
-		ID:       u.ID,
-		Fullname: u.Fullname,
-		Username: u.Username,
-		Email:    u.Email,
-	}})
+
+	data := BindResponse(&u)
+
+	c.JSON(http.StatusOK, gin.H{"data": data})
 }
 
 func (h *Handler) Create(c *gin.Context) {
 	var req dto.UserRequest
 
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"errors": helper.NewErrorResponse(err.Error())})
 		return
 	}
 
-	hashed, _ := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
-
-	u, err := h.service.Create(c, &entity.User{
-		Fullname: req.Fullname,
-		Username: req.Username,
-		Email:    req.Email,
-		Password: string(hashed),
-	})
+	u, err := h.service.Create(c, BindRequest(&req))
 
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"errors": helper.NewErrorResponse(err.Error())})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"data": dto.Response{
-		ID:       u.ID,
-		Fullname: u.Fullname,
-		Username: u.Username,
-		Email:    u.Email,
-	}})
+	data := BindResponse(&u)
+
+	c.JSON(http.StatusCreated, gin.H{"data": data})
 }
 
 func (h *Handler) Update(c *gin.Context) {
