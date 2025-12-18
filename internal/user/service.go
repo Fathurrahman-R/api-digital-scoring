009db6ea535800bfbd6e47efd96198eb08c60e92
Fathurrahman-R <mr.darksot123@gmail.com>
2025-12-18 10:19:47 +0700

    fix: change return type of entity pointer to entity new value

--- internal/user/service.go
+++ internal/user/service.go
@@ -2,7 +2,10 @@ package user
 
 import (
 	"api-digital-scoring/internal/entity"
+	"api-digital-scoring/internal/user/dto"
 	"context"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Service struct {
@@ -15,24 +18,24 @@ func NewService(repo Repository) *Service {
 	}
 }
 
-func (s *Service) GetByUsername(ctx context.Context, username string) (*entity.User, error) {
+func (s *Service) GetByUsername(ctx context.Context, username string) (entity.User, error) {
 	u, err := s.repo.GetByUsername(ctx, username)
 	if err != nil {
-		return nil, err
+		return u, err
 	}
 	return u, nil
 }
 
-func (s *Service) GetById(ctx context.Context, id string) (*entity.User, error) {
+func (s *Service) GetById(ctx context.Context, id string) (entity.User, error) {
 	u, err := s.repo.GetById(ctx, id)
 	if err != nil {
-		return nil, err
+		return u, err
 	}
 	return u, nil
 }
 
-func (s *Service) Create(ctx context.Context, user *entity.User) (*entity.User, error) {
-	u := &entity.User{
+func (s *Service) Create(ctx context.Context, user entity.User) (entity.User, error) {
+	u := entity.User{
 		Fullname: user.Fullname,
 		Username: user.Username,
 		Email:    user.Email,
@@ -41,14 +44,14 @@ func (s *Service) Create(ctx context.Context, user *entity.User) (*entity.User, 
 
 	object, err := s.repo.Create(ctx, u)
 	if err != nil {
-		return nil, err
+		return object, err
 	}
 
 	return object, nil
 }
 
-func (s *Service) Update(ctx context.Context, user *entity.User) (*entity.User, error) {
-	u := &entity.User{
+func (s *Service) Update(ctx context.Context, user entity.User) (entity.User, error) {
+	u := entity.User{
 		Fullname: user.Fullname,
 		Username: user.Username,
 		Email:    user.Email,
@@ -57,7 +60,7 @@ func (s *Service) Update(ctx context.Context, user *entity.User) (*entity.User, 
 
 	object, err := s.repo.Update(ctx, u)
 	if err != nil {
-		return nil, err
+		return object, err
 	}
 
 	return object, nil
@@ -70,4 +73,25 @@ func (s *Service) Delete(ctx context.Context, id string) error {
 	}
 
 	return nil
+}
+
+func BindRequest(request *dto.UserRequest) entity.User {
+	hashed, _ := bcrypt.GenerateFromPassword([]byte(request.Password), bcrypt.DefaultCost)
+	return entity.User{
+		Fullname: request.Fullname,
+		Username: request.Username,
+		Email:    request.Email,
+		Password: string(hashed),
+	}
+}
+
+func BindResponse(user *entity.User) []dto.Response {
+	return []dto.Response{
+		{
+			ID:       user.ID,
+			Fullname: user.Fullname,
+			Username: user.Username,
+			Email:    user.Email,
+		},
+	}
 }
